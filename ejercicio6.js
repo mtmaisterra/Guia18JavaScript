b557db2a17937b5cc3613cc2a8a4cf24fad4dd98
Teresa Maisterra <mtmaisterra@gmail.com>
2023-08-15 21:23:51 -0300

    commit 5

--- ejercicio6.js
+++ ejercicio6.js
@@ -2,3 +2,14 @@
 Realizar un programa que, dado un número entero, visualice en pantalla si es par o impar. 
 En caso de que el valor ingresado sea 0, se debe mostrar “el número no es par ni impar”. */
 
+
+var num = parseFloat(prompt("Ingresa el número:"));
+
+if (num == 0) {
+   alert (`el número no es par ni impar`);
+} else {
+    if(num %2 == 0){
+        alert(`El número es par`);
+    }
+    alert(`el número es impar`);
+}
