06b2a3c1fd53e38d4563ffddcbb60ff499aeab1a
Teresa Maisterra <mtmaisterra@gmail.com>
2023-08-16 22:05:07 -0300

    commit 10

---
+++ ejercicio8b.js
@@ -0,0 +1,30 @@
+/*Ejercicio 8
+Escribir un programa que lea números enteros hasta teclear 0 (cero). Al finalizar el 
+programa se debe mostrar el máximo número ingresado, el mínimo, y el promedio de 
+todos ellos. */
+var cant = 0;
+var suma = 0;
+let arr = [];
+var valorMax = 0;
+var prom = 0;
+var i = 0;
+
+do {
+    var num = parseFloat(prompt("Ingrese un número"));
+   
+    console.log("[ " + i + " ] = " + num);
+    i = i + 1;
+  suma = suma + num;
+  prom = suma / i;
+  if (num > valorMax){
+valorMax = num;
+  } /*Hasta acá funciona bien*/
+
+} while (num !== 0);
+
+ 
+
+console.log("La cantidad de números ingresados es: " + i);
+console.log("El valor máximo es: " + valorMax);
+console.log("El valor mínimo es: " + Math.min(num));/*Revisar*/
+console.log("El promedio es: " + prom);
